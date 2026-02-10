4073891ee90b173f3522ba60571eb5212781a6ba
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-02-10 16:52:32 -0500

    Added State of the model

--- backend/src/graph/state.py
+++ backend/src/graph/state.py
@@ -3,3 +3,34 @@ Docstring for ComplianceQAPipeline.backend.src.graph.state
 This file defines the state management for the graph pipeline
 
 """
+
+import operator
+from typing import TypedDict,Optional,Dict,Any,Annotated, List
+
+# Define schema for a compliance state
+class ComplianceState(TypedDict):
+    category: str
+    description: str
+    severity: str
+    timestamp: str
+
+class VideoState(TypedDict):
+    video_url: str
+    video_id: str
+
+    video_meta_data:Dict[str, Any]
+    transcript: Optional[str]
+    ocr_text: List[str]
+
+    compliance_report: Annotated[ComplianceState, operator.add]
+
+
+    final_status: str
+    final_report: str
+
+    error_report: Annotated[List[str], operator.add]
+
+
+
+
+
