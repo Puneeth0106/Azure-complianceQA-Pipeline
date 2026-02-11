4446b8c845e15fed375f720d49674e11de73c14c
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-02-11 11:53:35 -0500

    Modified state

--- backend/src/graph/state.py
+++ backend/src/graph/state.py
@@ -7,27 +7,33 @@ This file defines the state management for the graph pipeline
 import operator
 from typing import TypedDict,Optional,Dict,Any,Annotated, List
 
-# Define schema for a compliance state
-class ComplianceState(TypedDict):
+# Define schema for a compliance issue state
+class ComplianceIssue(TypedDict):
     category: str
     description: str
     severity: str
     timestamp: str
 
 class VideoState(TypedDict):
+
+    #Input features
     video_url: str
     video_id: str
 
-    video_meta_data:Dict[str, Any]
+    #Ingestion and extraction data
+    local_file_path: Optional[str]
+    video_metadata:Dict[str, Any]
     transcript: Optional[str]
     ocr_text: List[str]
 
-    compliance_report: Annotated[ComplianceState, operator.add]
+    #Analysis outpt
+    compliance_report: Annotated[List[ComplianceIssue], operator.add]
 
-
+    #Final deliverables
     final_status: str
     final_report: str
 
+    #System observability
     error_report: Annotated[List[str], operator.add]
 
 
