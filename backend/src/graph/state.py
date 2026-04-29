53f2b62c6ad756ac20428f58a3c5a321e0e14852
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-04-29 11:15:06 -0400

    Updated state module with explanation

--- backend/src/graph/state.py
+++ backend/src/graph/state.py
@@ -9,31 +9,38 @@ from typing import TypedDict,Optional,Dict,Any,Annotated, List
 
 # Define schema for a compliance issue state
 class ComplianceIssue(TypedDict):
+    """
+    Defines the schema for output compliance 
+    """
     category: str
-    description: str
-    severity: str
-    timestamp: str
+    description: str # specific detail of violation
+    severity: str # critical | warning
+    timestamp: Optional[str]
 
-class VideoState(TypedDict):
+class VideoAuditState(TypedDict):
+    """
+    Defines the schema of state that flows through langgraph
+    """
 
     #Input features
-    video_url: str
-    video_id: str
+    video_url: str 
+    video_id: str #import for videoindexer
 
     #Ingestion and extraction data
-    local_file_path: Optional[str]
-    video_metadata:Dict[str, Any]
-    transcript: Optional[str]
-    ocr_text: List[str]
+    local_file_path: Optional[str] #stores the temporary file 
+    video_metadata:Dict[str, Any] #{'duration':12,'file_name':'temp_file'}
+    transcript: Optional[str] #text extracted from speech to text
+    ocr_text: List[str] #text extracted from video using computer vision model
 
     #Analysis outpt
-    compliance_report: Annotated[List[ComplianceIssue], operator.add]
+    #stores the list of compliances and using operator.add it appends to the list rather than replacing it
+    compliance_report: Annotated[List[ComplianceIssue], operator.add] 
 
     #Final deliverables
-    final_status: str
+    final_status: str 
     final_report: str
 
-    #System observability
+    #System observability (captures the list of system level crashes)
     error_report: Annotated[List[str], operator.add]
 
 
